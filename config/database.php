2745e0beffb9c9ff50707cd3003bf70629d2839c
root <root@ubuntu.members.linode.com>
2016-08-23 23:35:09 +0000

    this fixes config stuff

--- config/database.php
+++ config/database.php
@@ -54,12 +54,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
+            'unix_socket' => '/var/run/mysqld/mysqld.sock',
             'database' => env('DB_DATABASE', 'beautyenc'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'password' => env('DB_PASSWORD', 'willy188'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
